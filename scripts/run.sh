581228f2a9531bdef0047809a436afd762488a1b
greeid <deekshit.bti@gmail.com>
2023-07-20 21:00:38 +0545

    `refac` : refactored to my liking

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,9 +2,14 @@
 
 xrdb merge ~/.Xresources 
 xbacklight -set 10 &
-feh --bg-fill ~/Pictures/wall/gruv.png &
+nitrogen --restore &
 xset r rate 200 50 &
-picom &
+discord &
+spotify-launcher &
+picom --config ~/.config/picom/picom.conf &
 
 ~/.config/chadwm/scripts/bar.sh &
+sleep 7
+nm-applet &
 while type dwm >/dev/null; do dwm && continue || break; done
+
