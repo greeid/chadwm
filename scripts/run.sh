e684329c46bca26d15aecc8163475b508e76f043
greeid <deekshit.bti@gmail.com>
2023-07-25 20:01:20 +0545

    `refac` : minor refac

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,11 +5,11 @@ discord &
 xbacklight -set 10 &
 nitrogen --restore &
 xset r rate 200 50 &
-spotify-launcher &
-volctl &
 picom --config ~/.config/picom/picom.conf &
-
+spotify-launcher &
 ~/.config/chadwm/scripts/bar.sh &
+volctl &
 nm-applet &
+
 while type dwm >/dev/null; do dwm && continue || break; done
 
